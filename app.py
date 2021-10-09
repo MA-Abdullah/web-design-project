704b45c67923232c9dee9b6b643cd524b983acd5
MA <new.05+1@icloud.com>
2021-10-09 10:34:18 +0300

    update

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return render_template('')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
