f2f33932e92cec8b4c7e60288195f4ea9b3d33f7
MA <new.05+1@icloud.com>
2021-10-26 02:27:31 +0300

    update

--- app.py
+++ app.py
@@ -6,9 +6,37 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/reservation")
+def reservation():
+    return render_template('templates/reservation.html')
+
+@app.route("/")
+def contactus():
+    return render_template('templates/contactus.html')
+
+
+@app.route("/")
+def aboutus():
+    return render_template('templates/aboutus.html')
+
+
+
+@app.route("/")
+def regestration():
+    return render_template('templates/regestration.html')
+
+
+
+
+@app.route("/")
+def login():
+    return render_template('templates/login.html')
+
+
+
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
